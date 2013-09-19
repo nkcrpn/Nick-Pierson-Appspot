d2fe2453bfe0f57717120b9bb64753b66514e6dd
Nick Pierson <nkcrpn@gmail.com>
2013-09-19 01:04:21 -0500

    All pages blog, blog/newpost, blog/id are now working. Home page for blog is styled.

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from python.handler import Handler
 from python.rot13 import Rot13
 from python.signup import *
 from python.asciichan import AsciiChan
-from python.blog import Blog
+from python.blog import *
 
 class MainPage(Handler):
     def get(self):
         self.render("main.html")
 
 application = webapp2.WSGIApplication(
-    [('/', MainPage), ('/rot13', Rot13), ('/signup', SignUp),
-     ('/welcome', Welcome), ('/ascii', AsciiChan), ('/blog', Blog),
-     ('/blog/newpost', Blog)], debug = True)
+    [(r'/', MainPage), (r'/rot13', Rot13), (r'/signup', SignUp),
+     (r'/welcome', Welcome), (r'/ascii', AsciiChan), (r'/blog', Blog),
+     (r'/blog/newpost', NewPost), (r'/blog/(\d+)', BlogPost)], debug = True)
--- python/blog.py
+++ python/blog.py
@@ -8,4 +8,36 @@ class Post(db.Model):
 
 class Blog(Handler):
     def get(self):
-        self.render("blog.html")
+        posts = db.GqlQuery("SELECT * FROM Post "
+                            "ORDER BY created Desc")
+        self.render("blog.html", posts=posts)
+
+class NewPost(Handler):
+    def render_page(self, title="", message="", error=""):
+        self.render("new_post.html", title=title, message=message,
+                    error=error)
+
+    def get(self):
+        self.render_page()
+
+    def post(self):
+        title = self.request.get("title")
+        message = self.request.get("message")
+
+        if title and message:
+            post = Post(title=title, message=message)
+            post.put()
+
+            self.redirect("/blog/" + str(post.key().id()))
+        else:
+            error = "Please enter both a title and message!"
+            self.render_page(title, message, error)
+
+class BlogPost(Handler):
+    def render_page(self, blog_id=None):
+        blog_post = Post.get_by_id(blog_id, parent=None)
+        self.render("blog_post.html", blog_post=blog_post,
+                    created_date=blog_post.created.date)
+
+    def get(self, blog_id):
+        self.render_page(int(blog_id))
